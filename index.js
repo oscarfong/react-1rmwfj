55dc91090961d4263482d1f5a1172c04e4ec7780
oscarfong <oscar.fong@gmail.com>
2019-05-20 20:17:25 +0800

    1 file modified, 1 added

--- index.js
+++ index.js
@@ -20,6 +20,26 @@ class App extends Component {
           <p/>
           Test to see if stackblitz reverts my code.
         </p>
+
+        <script type="text/javascript" src="tau-prolog.js"></script>
+        <script>
+
+        var session = pl.create();
+
+        var parsed = session.consult("
+        % load lists module
+        :- use_module(library(lists)).
+
+        % fruit/1
+        fruit(apple). fruit(pear). fruit(banana).
+
+        % fruits_in/2
+        fruits_in(Xs, X) :- member(X, Xs), fruit(X).
+        "); // true
+        if ( parsed != true ) 
+          console.log( parsed ); 
+        
+        </script>
       </div>
     );
   }
